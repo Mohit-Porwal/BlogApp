692da1c14cb8468b156fb5109faca1d330bfd7ef
L-E-A-R-N-E-R <porwalmohit1999@gmail.com>
2023-12-07 20:32:08 -0800

    Created comment entity and comment repository

--- springboot-blog-rest-api/src/main/java/com/springboot/blog/entity/Post.java
+++ springboot-blog-rest-api/src/main/java/com/springboot/blog/entity/Post.java
@@ -6,6 +6,9 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.boot.autoconfigure.web.WebProperties;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -23,4 +26,7 @@ public class Post {
     private String title;
     private String description;
     private String content;
+
+    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<Comment> comments = new HashSet<>();
 }
